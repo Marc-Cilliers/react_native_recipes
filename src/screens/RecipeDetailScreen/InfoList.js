3493b22541b98c191f12242207b3b01ca7791958
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 18:32:37 +0200

    :construction: Add basic logic to InfoList

--- src/screens/RecipeDetailScreen/InfoList.js
+++ src/screens/RecipeDetailScreen/InfoList.js
@@ -3,17 +3,50 @@ import {StyleSheet, View, Text} from 'react-native';
 
 type Props = {
   title: string,
-  content: Array<string>,
+  content: Array<any>,
   numbered: boolean,
 };
-const InfoList = ({title, content}): Object => {
+const InfoList = ({title, content, numbered}): Object => {
+  const ContentList = () => {
+    content.map((c, i) => {
+      return (
+        <View style={styles.row}>
+          {numbered && <Text>{i + 1}</Text>}
+          {!numbered && <Text>•</Text>}
+          <Text>c.ingredient</Text>
+          <Text>c.amount {c.orAmount && `| ${c.orAmount}`}</Text>
+        </View>
+      );
+    });
+  };
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>{title}</Text>
+      <View style={styles.seperator} />
+
+      <ContentList />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  seperator: {
+    height: 1,
+    width: '90%',
+    backgroundColor: 'grey',
+  },
+});
 
 export default InfoList;
