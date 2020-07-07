770a738bd9aaac2cd88ccfcf7d4058926be00598
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 19:00:00 +0200

    :art: Change content prop to a string array

--- src/screens/RecipeDetailScreen/InfoList.js
+++ src/screens/RecipeDetailScreen/InfoList.js
@@ -3,7 +3,7 @@ import {StyleSheet, View, Text} from 'react-native';
 
 type Props = {
   title: string,
-  content: Array<any>,
+  content: Array<string>,
   numbered: boolean,
 };
 
@@ -15,10 +15,9 @@ const InfoList = ({title, content, numbered}: Props): Object => {
 
       {content.map((c, i) => (
         <View key={i} style={styles.row}>
-          {numbered && <Text>{i + 1}</Text>}
+          {numbered && <Text>{i + 1}.)</Text>}
           {!numbered && <Text>•</Text>}
-          <Text>c.ingredient</Text>
-          <Text>c.amount {c.orAmount && `| ${c.orAmount}`}</Text>
+          <Text>{c}</Text>
         </View>
       ))}
     </View>
@@ -28,17 +27,20 @@ const InfoList = ({title, content, numbered}: Props): Object => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
+    marginTop: 20,
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    marginBottom: 5,
+    justifyContent: 'flex-start',
   },
   header: {
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: '700',
   },
   seperator: {
     height: 1,
+    marginBottom: 20,
     width: '90%',
     backgroundColor: 'grey',
   },
