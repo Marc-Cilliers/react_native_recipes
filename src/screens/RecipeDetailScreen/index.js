a06021b9c4b0fa29eff0c1f5bd55b78b38e140b4
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 17:30:14 +0200

    :lipstick: Add image jumbotron with spinner

--- src/screens/RecipeDetailScreen/index.js
+++ src/screens/RecipeDetailScreen/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Image, ActivityIndicator} from 'react-native';
 
-const RecipeDetailScreen = (): Object => {};
+type Props = {
+  navigation: any,
+  route: any,
+};
 
-const styles = StyleSheet.create({});
+const RecipeDetailScreen = ({navigation, route}: Props): Object => {
+  const {recipe}: {recipe: Recipe} = route.params;
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <View style={styles.jumbotron}>
+      <Image
+        source={{uri: recipe.imageUrl}}
+        onLoadEnd={() => setLoading(false)}
+      />
+      <ActivityIndicator style={styles.spinner} animating={loading} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  jumbotron: {
+    height: 300,
+    width: '100%',
+  },
+  spinner: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default RecipeDetailScreen;
