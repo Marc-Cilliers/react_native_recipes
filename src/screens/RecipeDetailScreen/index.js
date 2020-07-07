578bb7dec5987efc3a7d36db3a315fb4d13fbb9a
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 19:00:28 +0200

    :art: Change format of ingredients passed as prop

--- src/screens/RecipeDetailScreen/index.js
+++ src/screens/RecipeDetailScreen/index.js
@@ -1,7 +1,14 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Image, ActivityIndicator} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  ActivityIndicator,
+  SafeAreaView,
+} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import InfoBadge from './InfoBadge';
+import InfoList from './InfoList';
 
 type Props = {
   navigation: any,
@@ -12,22 +19,39 @@ const RecipeDetailScreen = ({navigation, route}: Props): Object => {
   const {recipe}: {recipe: Recipe} = route.params;
   const [loading, setLoading] = useState(true);
 
-  return (
-    <ScrollView>
-      <View style={styles.jumbotron}>
-        <Image
-          style={styles.image}
-          source={{uri: recipe.imageUrl}}
-          onLoadEnd={() => setLoading(false)}
-        />
-        <ActivityIndicator style={styles.spinner} animating={loading} />
-      </View>
+  const ingredients = recipe.ingredients.map((i) => {
+    if (i.orAmount) {
+      return `${i.ingredient} (${i.amount} | ${i.orAmount})`;
+    }
+    return `${i.ingredient} (${i.amount})`;
+  });
 
-      <View style={styles.badges}>
-        <InfoBadge icon={'\uf823'} text={`${recipe.servings} servings`} />
-        <InfoBadge icon={'\uf251'} text={`${recipe.cookingTime} min`} />
-      </View>
-    </ScrollView>
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.jumbotron}>
+          <Image
+            style={styles.image}
+            source={{uri: recipe.imageUrl}}
+            onLoadEnd={() => setLoading(false)}
+          />
+          <ActivityIndicator style={styles.spinner} animating={loading} />
+        </View>
+
+        <View style={styles.badges}>
+          <InfoBadge icon={'\uf823'} text={`${recipe.servings} servings`} />
+          <InfoBadge icon={'\uf251'} text={`${recipe.cookingTime} min`} />
+        </View>
+
+        <InfoList
+          title={'Ingredients'}
+          content={ingredients}
+          numbered={false}
+        />
+
+        <InfoList title={'Method'} content={recipe.method} numbered={true} />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
