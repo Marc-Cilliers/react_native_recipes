fcb38a66e982c77da44a0b08feceb19f8fe9c5aa
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 18:10:55 +0200

    :sparkles: Add badges section

--- src/screens/RecipeDetailScreen/index.js
+++ src/screens/RecipeDetailScreen/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {StyleSheet, View, Image, ActivityIndicator} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
+import InfoBadge from './InfoBadge';
 
 type Props = {
   navigation: any,
@@ -21,6 +22,11 @@ const RecipeDetailScreen = ({navigation, route}: Props): Object => {
         />
         <ActivityIndicator style={styles.spinner} animating={loading} />
       </View>
+
+      <View style={styles.badges}>
+        <InfoBadge icon={'\uf823'} text={`${recipe.servings} servings`} />
+        <InfoBadge icon={'\uf251'} text={`${recipe.cookingTime} min`} />
+      </View>
     </ScrollView>
   );
 };
@@ -29,6 +35,9 @@ const styles = StyleSheet.create({
   jumbotron: {
     height: 300,
     width: '100%',
+  },
+  badges: {
+    flexDirection: 'row',
   },
   image: {
     height: '100%',
