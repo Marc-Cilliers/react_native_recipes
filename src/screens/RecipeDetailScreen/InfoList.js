12c9f77762ca7c41ff4072f1d3b62b77c6ab87cd
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 18:21:42 +0200

    :truck: Rename InfoSection to InfoList for clarity

---
+++ src/screens/RecipeDetailScreen/InfoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+type Props = {
+  title: string,
+  content: Array<string>,
+  numbered: boolean,
+};
+const InfoList = ({title, content}): Object => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default InfoList;
--- src/screens/RecipeDetailScreen/InfoSection.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-
-const InfoSection = (): Object => {};
-
-const styles = StyleSheet.create({});
-
-export default InfoSection;
