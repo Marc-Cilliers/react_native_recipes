eef9bb049c8fe0e28779c3e9066bd6a884385e11
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 17:33:42 +0200

    :sparkles: Add navigation from List screen to Detail screen

--- src/screens/RecipeListScreen/index.js
+++ src/screens/RecipeListScreen/index.js
@@ -5,15 +5,21 @@ import {StyleSheet, FlatList} from 'react-native';
 import api from '../../services/api';
 import RecipeList from './RecipeList';
 
-type Props = {};
-const RecipeListScreen = ({}: Props): Object => {
+type Props = {
+  navigation: any,
+};
+const RecipeListScreen = ({navigation}: Props): Object => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     api.getAllRecipes().then((allRecipes) => setRecipes(allRecipes));
   }, []);
 
-  const recipeTapHandler = () => {};
+  const recipeTapHandler = (recipe) => {
+    navigation.navigate('RecipeDetail', {
+      recipe,
+    });
+  };
 
   return <RecipeList recipes={recipes} onRecipeTap={recipeTapHandler} />;
 };
