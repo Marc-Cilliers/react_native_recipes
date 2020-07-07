7925e52a02f1f8dc07a19a94dd4d81d2411a8be4
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 15:59:29 +0200

    :construction: Add RecipeListScreen boilerplate

---
+++ src/screens/RecipeListScreen/index.js
@@ -0,0 +1,10 @@
+// @flow
+
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const RecipeListScreen = (): Object => {};
+
+const styles = StyleSheet.create({});
+
+export default RecipeListScreen;
