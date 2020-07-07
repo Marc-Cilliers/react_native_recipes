a568b673238bbb80be8bd4cea984adf0733f79ac
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 17:19:50 +0200

    :construction: Add RecipeDetailScreen boilerplate

---
+++ src/screens/RecipeDetailScreen/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const RecipeDetailScreen = (): Object => {};
+
+const styles = StyleSheet.create({});
+
+export default RecipeDetailScreen;
