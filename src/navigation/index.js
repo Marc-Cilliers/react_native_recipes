6d4bad97df66f794d993780a3d3e1ad5ea55281e
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 16:41:52 +0200

    :construction: Add basic stack navigation

---
+++ src/navigation/index.js
@@ -0,0 +1,19 @@
+//@flow
+
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import RecipeListScreen from '../screens/RecipeListScreen';
+
+const Stack = createStackNavigator();
+
+const RootStack = (): Object => {
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Recipes">
+      <Stack.Screen name="Recipes" component={RecipeListScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>;
+};
+
+export default RootStack;
