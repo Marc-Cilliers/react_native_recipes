29a08ddd466b7609c0e68cfac7e8fb6472b5e4f3
Marc-Cilliers <mcilliers@arbnco.com>
2020-07-07 16:34:07 +0200

    :sparkles: Add basic FlatList with react-elements ListItem

--- src/screens/RecipeListScreen/RecipeList.js
+++ src/screens/RecipeListScreen/RecipeList.js
@@ -1,7 +1,33 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+//@flow
 
-const RecipeList = (): Object => {};
+import React from 'react';
+import {StyleSheet, FlatList} from 'react-native';
+import type {Recipe} from '../../models/recipe';
+import {ListItem} from 'react-native-elements';
+
+type Props = {
+  recipes: Array<Recipe>,
+  onRecipeTap: Object,
+};
+
+const RecipeList = ({recipes, onRecipeTap}: Props): Object => {
+  const _keyExtractor = (item, index) => index.toString();
+
+  return (
+    <FlatList
+      data={recipes}
+      keyExtractor={_keyExtractor}
+      renderItem={(item: Recipe, index) => (
+        <ListItem
+          onPress={() => onRecipeTap(item)}
+          leftAvatar={{source: {uri: item.imageUrl}}}
+          title={item.title}
+          bottomDivider
+        />
+      )}
+    />
+  );
+};
 
 const styles = StyleSheet.create({});
 
